2f7ec099eb25fbbf8015eb30611630a465672f2f
Wise <38019101+WiseChannel@users.noreply.github.com>
2019-04-26 16:10:46 +0300

    Fcom

---
+++ precache-manifest.4eef265728374a6dd74aa8754ebf581a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "8328d225dec4b9616bc1",
+    "url": "/static/js/main.8328d225.chunk.js"
+  },
+  {
+    "revision": "ed3414b097ec5d5000be",
+    "url": "/static/js/2.ed3414b0.chunk.js"
+  },
+  {
+    "revision": "8328d225dec4b9616bc1",
+    "url": "/static/css/main.278f180b.chunk.css"
+  },
+  {
+    "revision": "1a4a5b8d240d81cbfcfde25253ca3897",
+    "url": "/index.html"
+  }
+];
